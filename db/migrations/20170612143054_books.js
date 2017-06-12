b470b45da109dc34fa7b2ed6f1322d4196c11dc0
Grant Willison <emailgdw@gmail.com>
2017-06-12 15:42:05 -0700

    Finished part 1

---
+++ db/migrations/20170612143054_books.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('books', (table) => {
+  table.increments('id').primary();  // NOTE syntax unsure remove if passes
+  table.string('title').notNullable().defaultTo('');
+  table.string('author').notNullable().defaultTo('');
+  table.string('genre').notNullable().defaultTo('');
+  table.text('description').notNullable().defaultTo('');
+  table.text('cover_url').notNullable().defaultTo('');
+
+  table.timestamps(true, true);
+})
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('books')
+  .then()
+  .catch()
+};
