f58549a59a79473ef6c5d5f8eec5134f84256eae
Dave <wm.david.gallup@gmail.com>
2017-06-18 10:33:09 -0700

    Made notes

---
+++ heroku_notes.sh
@@ -0,0 +1,22 @@
+brew install heroku
+
+heroku create grant-dave-galvanize-bookshelf
+
+heroku config:set 'JWT_KEY=5AhdmefVs8NL2sx1creXCeRoiOEXfPZKFL6fY6v5/xRbqXXnvLKN6QZ7wlx0urxG TCzLxm9gaWJNSwysp4/rjg=='
+
+heroku addons:create heroku-postgresql:hobby-dev
+
+git push heroku master (?)
+
+(then add hreoku-postbuild script to package.json.  Already there for us...)
+"scripts": {
+  "heroku-postbuild": "knex migrate:latest",
+  ...
+)
+
+heroku run bash
+npm run knex seed:run
+exit
+
+heroku open
+
